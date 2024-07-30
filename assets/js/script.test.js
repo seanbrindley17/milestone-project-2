a44e5647f8a2aebced27dafc34ac974c513be02d
Sean Brindley <seanbrindley17@gmail.com>
2024-07-30 14:20:22 +0100

    Add test page for later use

--- assets/js/script.test.js
+++ assets/js/script.test.js
@@ -5,10 +5,3 @@ beforeAll(() => {
     document.write(fileContents);
     document.close();
 });
-
-describe("game works correctly upon clicking play", () => {
-    beforeAll(() => {
-        game.score = 5
-        game.computerCardSelection = [""]
-    })
-})
